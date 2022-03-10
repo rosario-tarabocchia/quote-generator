b3f89bd34d25a30e08933260d9f0233ba0c2b962
rtaraboo <rtarabocchia@gmail.com>
2022-03-10 18:44:03 -0500

    Prepared script for heroku

--- public/script.js
+++ public/script.js
@@ -1,19 +1,14 @@
+// Loader Script
+
 const newQuoteButton = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
 const quoteContainer = document.getElementById("quote-container");
-// loadingComplete();
+
 loader.hidden = true;
 
 function loading() {
     loader.hidden = false;
     quoteContainer.hidden = true;
-
 };
-
-// function loadingComplete() {
-//     loader.hidden = true;
-//     quoteContainer.hidden = false;
-
-// };
 
 newQuoteButton.addEventListener("click", loading);
